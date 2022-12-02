09e514327062edccb6fc02e60affbae30890054c
bertwinkeller <bertwinkeller@gmail.com>
2022-12-02 14:25:51 -0800

    sidebar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo} from '../utils/constants'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
   return(
@@ -10,6 +11,7 @@ const Navbar = () => {
       <img src={logo} height={45} alt="logo" />
       </Link>
      
+     <SearchBar />
     </Stack>
   )
 }
