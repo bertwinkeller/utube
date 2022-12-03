c609abbbc00bf933ec7b88313a17052dfc269d9e
bertwinkeller <bertwinkeller@gmail.com>
2022-12-02 22:42:35 -0800

    api now working

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,9 +2,8 @@ import React from "react"
 import { Stack } from "@mui/material"
 import { categories } from "../utils/constants"
 
-const selectedCategory = "New"
 
-const Sidebar = () => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   return (
     <Stack
       direction="row"
@@ -17,7 +16,7 @@ const Sidebar = () => {
       {categories.map((category) => (
         <button
           className="category-btn"
-          //   onClick={() => setSelectedCategory(category.name)}
+            onClick={() => setSelectedCategory(category.name)}
           style={{
             background: category.name === selectedCategory && "#FC1503",
             color: "white",
